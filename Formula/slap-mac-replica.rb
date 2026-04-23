1c259b65f32249e8c2deb105caee3fe2645be0e7
heodongun <162291579+heodongun@users.noreply.github.com>
2026-04-23 10:35:04 +0900

    fix(release): point formula at v0.1.4 asset

--- Formula/slap-mac-replica.rb
+++ Formula/slap-mac-replica.rb
@@ -1,8 +1,8 @@
 class SlapMacReplica < Formula
   desc "Play a sound when you physically slap an Apple Silicon MacBook"
   homepage "https://github.com/bssm-oss/slap-mac-replica"
-  url "https://github.com/bssm-oss/slap-mac-replica/releases/download/v0.1.3/slap-mac-replica_0.1.3_darwin_arm64.tar.gz"
-  sha256 "790cdf4c757bbc037ce039aa3e463c57e819f66123be2394e3e1810a52242c77"
+  url "https://github.com/bssm-oss/slap-mac-replica/releases/download/v0.1.4/slap-mac-replica_0.1.4_darwin_arm64.tar.gz"
+  sha256 "68865d067a296923dabce873bb2db92eb4f5363d43300b4dc895c13582604f03"
   license "MIT"
 
   def install
